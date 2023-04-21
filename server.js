bbf25a7fea234e9b4cf8ddb3f745aef1d7043ea2
Tuệ Hồ <tueho>
2023-04-21 21:30:53 +0700

    fix

--- server.js
+++ server.js
@@ -38,86 +38,6 @@ const connectDB = () => {
 connectDB();
 
 var listChatID = [];
-var options = [
-  {
-    textKeoTX: "đây là text kèo tài xỉu",
-    textKeoChap: "đây là text kèo  kèo chấp",
-    doi1: "Đội 1: ",
-    doi2: "Đội 2 ",
-    keoTX: "2.5",
-    keoChap: "2.0",
-    createDate: "2023-04-21T13:15:08.405Z",
-    text: "11313",
-    __v: 0,
-  },
-  {
-    textKeoTX: "1",
-    textKeoChap: "2",
-    doi1: "5",
-    doi2: "7",
-    keoTX: "3",
-    keoChap: "4",
-    createDate: "2023-04-21T10:56:06.024Z",
-    text: "11313",
-
-    __v: 0,
-  },
-  {
-    textKeoTX: "1",
-    textKeoChap: "2",
-    doi1: "5",
-    doi2: "7",
-    keoTX: "3",
-    keoChap: "4",
-    createDate: "2023-04-21T10:55:37.424Z",
-    text: "11313",
-
-    __v: 0,
-  },
-];
-
-var keyboard = {
-  inline_keyboard: [
-    [
-      {
-        textKeoTX: "đây là text kèo tài xỉu",
-        textKeoChap: "đây là text kèo  kèo chấp",
-        doi1: "Đội 1: ",
-        doi2: "Đội 2 ",
-        keoTX: "2.5",
-        keoChap: "2.0",
-        createDate: "2023-04-21T13:15:08.405Z",
-        text: "11313",
-        __v: 0,
-        callback_data: "12",
-      },
-      {
-        textKeoTX: "1",
-        textKeoChap: "2",
-        doi1: "5",
-        doi2: "7",
-        keoTX: "3",
-        keoChap: "4",
-        createDate: "2023-04-21T10:56:06.024Z",
-        text: "11313",
-        callback_data: "12",
-        __v: 0,
-      },
-      {
-        textKeoTX: "1",
-        textKeoChap: "2",
-        doi1: "5",
-        doi2: "7",
-        keoTX: "3",
-        keoChap: "4",
-        createDate: "2023-04-21T10:55:37.424Z",
-        text: "11313",
-        callback_data: "12",
-        __v: 0,
-      },
-    ],
-  ],
-};
 
 app.get("/", (req, res) => {
   res.render("index", { options });
@@ -126,19 +46,25 @@ app.get("/", (req, res) => {
 // Handle the /start command
 
 // Handle callback queries
-bot.on("callback_query", (query) => {
+bot.on("callback_query", async (query) => {
   const { chat, message_id, text } = query.message;
 
-  console.log(query);
-  // const option = options.find((option) => option.callback_data === query.data);
+  const listKeo = await KeoToday.find()
+    .sort({ createDate: -1 })
+    .limit(3)
+    .then((result) => {
+      const option = result.find((item) => item.callback_data === query.data);
 
-  // bot.editMessageText(
-  //   `${text}\n\n"${option.keoChap}", kèo hôm nay   ${option.keoTX} chấp ${option.keoTX} 10 trái, chọn kèo ${option.keoTX}`,
-  //   {
-  //     chat_id: chat.id,
-  //     message_id: message_id,
-  //   }
-  // );
+      console.log(text);
+
+      bot.editMessageText(
+        `${text}\n\n"${option.callback_data}", kèo hôm nay   ${option.textKeoChap} chấp ${option.textKeoChap} 10 trái, chọn kèo ${option.textKeoChap}`,
+        {
+          chat_id: chat.id,
+          message_id: message_id,
+        }
+      );
+    });
 });
 
 bot.on("message", async (msg) => {
@@ -152,8 +78,6 @@ bot.on("message", async (msg) => {
         .then((result) => {
           const result1 = JSON.stringify(result);
           const result2 = JSON.parse(result1);
-
-          console.log(result);
 
           const reply_markup = {
             inline_keyboard: [result2],
