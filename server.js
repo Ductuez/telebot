aa5c0c351dbabea82998760def37632cc69b671d
Tue Ho <tueho@Tues-MacBook-Pro.local>
2023-04-21 17:01:03 +0700

    fix

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.get("/listKeo", async (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
